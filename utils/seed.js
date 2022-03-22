9fd93e31e7e8bb494b1f596876b28f9c4b9e986d
Yujen Chen <yujengogo@gmail.com>
2022-03-21 23:02:19 -0400

    changing seed file

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,5 @@
 const connection = require('../config/connection');
-const { Course, Student } = require('../models');
+const { User, Thought } = require('../models');
 const { getRandomName, getRandomAssignments } = require('./data');
 
 connection.on('error', (err) => err);
@@ -8,44 +8,42 @@ connection.once('open', async () => {
   console.log('connected');
 
   // Drop existing courses
-  await Course.deleteMany({});
+  await User.deleteMany({});
 
-  // Drop existing students
-  await Student.deleteMany({});
+  // Drop existing thoughts
+  await Thought.deleteMany({});
 
-  // Create empty array to hold the students
-  const students = [];
+  // Create empty array to hold the thoughts
+  const thoughts = [];
 
   // Get some random assignment objects using a helper function that we imported from ./data
   const assignments = getRandomAssignments(20);
 
-  // Loop 20 times -- add students to the students array
+  // Loop 20 times -- add thoughts to the thoughts array
   for (let i = 0; i < 20; i++) {
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
+    const username = getRandomName();
 
-    students.push({
-      first,
-      last,
-      github,
-      assignments,
+
+    thoughts.push({
+      thoughtText,
+      createdAt,
+      username,
+      reactions,
     });
   }
 
-  // Add students to the collection and await the results
-  await Student.collection.insertMany(students);
+  // Add thoughts to the collection and await the results
+  await Thought.collection.insertMany(thoughts);
 
   // Add courses to the collection and await the results
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
-    inPerson: false,
-    students: [...students],
+  await User.collection.insertOne({
+    username: 'Mike',
+    email: 'mike@mike.com',
+    thoughts: [...thoughts],
   });
 
   // Log out the seed data to indicate what should appear in the database
-  console.table(students);
+  console.table(thoughts);
   console.table(assignments);
   console.info('Seeding complete! 🌱');
   process.exit(0);
